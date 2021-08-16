377eaf28b8010540aed78603a838995144bd208e
Ben <ben.jackel@fcc-fac.ca>
2021-08-16 14:26:16 -0600

    investment calculator and associated tests

--- calculators/investment_calculator/investment_calculator.py
+++ calculators/investment_calculator/investment_calculator.py
@@ -1,21 +1,105 @@
+from datetime import date
 import pandas as pd
+import numpy as np
+from pandas.tseries.offsets import Milli
 
 
 class InvestmentCalculator:
-    def __init__(self, balance, dividend_yield, frequency):
+    def __init__(self, balance, dividend_yield, frequency, dividend_issue_date):
         self.balance = balance
         self.dividend_yield = dividend_yield
         self.frequency = frequency
+        self.dividend_balance = 0.0
+        self.dividend_issue_day = pd.to_datetime(dividend_issue_date).date()
 
     def __repr__(self):
         df = pd.DataFrame({"balance": [self.balance]})
         return repr(df)
 
     def buy(self, amount):
+        if amount < 0:
+            raise ValueError("Can't buy a negative amount")
+
+        self.balance += amount
         return self
 
     def sell(self, amount):
+        if amount < 0:
+            raise ValueError("Can't sell a negative amount")
+        if amount > self.balance:
+            raise ValueError("Insufficient balance")
+
+        self.balance -= amount
         return self
 
-    def extract_dividends(self, amount):
+    @staticmethod
+    def custom_date_range(start, end, freq, known_date):
+        """
+        Custom date range function
+        Specifies a start and an end range. That must contain start, end
+        Then, finds the next business day incase they land on a weekend
+
+        For example, quarterly frequency
+        start = 2021-08-10
+        end = 2022-08-10
+        2021-08-10, 2021-11-10, 2022-02-10, ..., 2022-08-10
+
+        """
+        day = known_date.day
+        month_abbr = pd.to_datetime(known_date).strftime("%b")
+        freqs = {
+            "monthly": "m",
+            "quarterly": f"Q-{month_abbr}",
+            "semi-annually" "annualy": f"A-{month_abbr}",
+        }
+        date_range = pd.date_range(
+            start=start, end=end + pd.DateOffset(years=1), freq=freqs.get(freq)
+        )
+        date_range = pd.Series(date_range).apply(lambda x: x + pd.DateOffset(day=day))
+        date_range = date_range + 0 * pd.offsets.BDay()
+        return date_range
+
+    def next_dividend_date(self, current_date):
+        this_date = pd.to_datetime(current_date).date()
+        dividend_date = self.dividend_issue_day
+
+        if this_date < dividend_date:
+            start = this_date
+            end = dividend_date
+        else:
+            start = dividend_date
+            end = this_date
+
+        dr = self.custom_date_range(
+            start=start, end=end, freq=self.frequency, known_date=dividend_date
+        )
+
+        return dr[dr >= np.datetime64(this_date)].min().date()
+
+    def issue_dividend(self, current_date):
+        if pd.to_datetime(current_date).date() == self.next_dividend_date(current_date):
+            self.dividend_balance += round(
+                self.balance * self.dividend_yield / 100 / 12.0, 2
+            )
+
         return self
+
+    def withdraw_dividends(self, amount):
+        if amount > self.dividend_balance:
+            raise ValueError("Insufficient dividend balance.")
+        if amount < 0:
+            raise ValueError("amount must be greater than 0")
+
+        self.dividend_balance -= amount
+
+        return self
+
+
+if __name__ == "__main__":
+
+    investment = InvestmentCalculator(
+        balance=120000,
+        dividend_yield=10,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
--- calculators/tests/test_investment_calculator.py
+++ calculators/tests/test_investment_calculator.py
@@ -1,14 +1,162 @@
 import pytest
+import pandas as pd
 from calculators.investment_calculator.investment_calculator import InvestmentCalculator
 
 
 def test_buy():
-    assert 1 == 0
+    investment = InvestmentCalculator(
+        balance=0.0,
+        dividend_yield=10.0,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
+    investment.buy(10000)
+    predicted = investment.balance
+    actual = 10000
+    assert predicted == actual
+
+    # Buy again
+    investment.buy(5000)
+    predicted = investment.balance
+    actual = 15000
+    assert predicted == actual
+
+    # Try to buy a negative amount
+    with pytest.raises(ValueError):
+        investment.buy(-100.0)
+
+    # Buy 0 should do nothing
+    investment.buy(0)
+    predicted = investment.balance
+    actual = 15000
+    assert predicted == actual
 
 
 def test_sell():
-    assert 1 == 0
+    investment = InvestmentCalculator(
+        balance=10000.0,
+        dividend_yield=10.0,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
+
+    investment.sell(5000)
+    predicted = investment.balance
+    actual = 5000
+    assert predicted == actual
+
+    # Sell again
+    investment.sell(1000)
+    predicted = investment.balance
+    actual = 4000
+    assert predicted == actual
+
+    # Try to buy a negative amount
+    with pytest.raises(ValueError):
+        investment.sell(-100.0)
+
+    # Buy 0 should do nothing
+    investment.sell(0)
+    predicted = investment.balance
+    actual = 4000
+    assert predicted == actual
+
+    # Try to sell more than there is
+    with pytest.raises(ValueError):
+        investment.sell(5000)
 
 
-def test_extract_dividends():
-    assert 1 == 0
+def test_next_dividend_date():
+    investment = InvestmentCalculator(
+        balance=100000.0,
+        dividend_yield=10.0,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
+
+    next_date = investment.next_dividend_date("2021-08-09")
+    actual = pd.to_datetime("2021-08-10").date()
+    assert next_date == actual
+
+    next_date = investment.next_dividend_date("2021-08-10")
+    actual = pd.to_datetime("2021-08-10").date()
+    assert next_date == actual
+
+    next_date = investment.next_dividend_date("2021-08-11")
+    actual = pd.to_datetime("2021-09-10").date()
+    assert next_date == actual
+
+    investment = InvestmentCalculator(
+        balance=100000.0,
+        dividend_yield=10.0,
+        frequency="quarterly",
+        dividend_issue_date="2021-10-10",
+    )
+
+    next_date = investment.next_dividend_date("2021-08-09")
+    actual = pd.to_datetime("2021-10-11").date()  # Oct 10th is a sunday
+    assert next_date == actual
+
+    next_date = investment.next_dividend_date("2021-10-10")
+    actual = pd.to_datetime("2021-10-11").date()
+    assert next_date == actual
+
+    next_date = investment.next_dividend_date("2021-11-11")
+    actual = pd.to_datetime("2022-01-10").date()
+    assert next_date == actual
+
+
+def test_issue_dividend():
+    investment = InvestmentCalculator(
+        balance=120000.0,
+        dividend_yield=10.0,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
+
+    investment.issue_dividend("2021-08-09")
+    balance = investment.dividend_balance
+    actual = 0.0
+    assert balance == actual
+
+    investment.issue_dividend("2021-08-10")
+    balance = investment.dividend_balance
+    actual = 1000.0
+    assert balance == actual
+
+    investment.issue_dividend("2021-08-11")
+    balance = investment.dividend_balance
+    actual = 1000.0
+    assert balance == actual
+
+    investment.issue_dividend("2021-09-10")
+    balance = investment.dividend_balance
+    actual = 2000.0
+    assert balance == actual
+
+    investment.issue_dividend("2021-10-11")
+    balance = investment.dividend_balance
+    actual = 3000.0
+    assert balance == actual
+
+
+def test_withdraw_dividends():
+    investment = InvestmentCalculator(
+        balance=120000.0,
+        dividend_yield=10.0,
+        frequency="monthly",
+        dividend_issue_date="2021-10-10",
+    )
+
+    investment.issue_dividend("2021-10-11")
+
+    investment.withdraw_dividends(10)
+    balance = investment.dividend_balance
+    actual = 990
+    assert balance == actual
+
+    with pytest.raises(ValueError):
+        investment.withdraw_dividends(-100)
+
+    with pytest.raises(ValueError):
+        investment.withdraw_dividends(10000)
